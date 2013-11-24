e00c147a3ad722d37df9f9aa5d04fb41b530eea3
Victor Martinez <vrmp00@gmail.com>
2013-11-24 00:22:38 +0000

    Fetch master happy/sad

--- proj/proyecto.R
+++ proj/proyecto.R
@@ -3,8 +3,6 @@
 ## Ejemplo: http://pages.stern.nyu.edu/~achinco/programming_examples/Example__PlotGeographicDensity.html
 ## RgoogleMaps: http://cran.r-project.org/web/packages/RgoogleMaps/index.html
 
----
-
 library(ggmap)
 library(ggplot2)
 g.map <- get_map(location = c(lon = -99.1393, lat = 19.3772), 
@@ -15,13 +13,16 @@ happy.coordinates <- read.csv(file="coord_felices.txt", header=F)
 names(happy.coordinates) <- c("x", "y")
 head(happy.coordinates)
 
-map + geom_point(data = happy.coordinates, 
-                 aes(x = x, y = y), colour = "blue", size = 1)
-
-
 sad.coordinates <- read.csv(file="coord_tristes.txt", header=F)
 names(sad.coordinates) <- c("x", "y")
 head(sad.coordinates)
 
-map + geom_point(data = sad.coordinates, 
-                 aes(x = x, y = y), colour = "red", size = 1)
+coord_map <- map + geom_point(data = happy.coordinates, aes(x = x, y = y), colour = "green", size = 2) + geom_point(data = sad.coordinates, aes(x=x, y = y), colour="red", size=2)
+coord_map
+
+#Dibujamos 2 clusters en el mapa (y uno por afuera)
+#TODO: revisar por q falla
+clusters <- kmeans(coordinates, 3, iter.max = 100, nstart=3)
+centers <- data.frame(x = clusters$centers[,1], y = clusters$centers[,2])
+cluster_map <- coord_map + geom_point(data = centers, aes(x=x, y=y), colour="blue", size=4)
+cluster_map
