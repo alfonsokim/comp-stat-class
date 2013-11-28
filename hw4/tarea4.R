a8ca81bb7e3fad8e5ec81b281771418319a5fd3b
Alfonso Kim <alfonsokim84@gmail.com>
2013-11-27 20:24:46 -0600

    avances tarea

--- hw4/tarea4.R
+++ hw4/tarea4.R
@@ -22,9 +22,49 @@ lm.model <- lm(maxO3 ~ T9 + T12 + T15 + Ne9 + Ne12 + Ne15 +
                    Wx9 + Wx12 + Wx15 + maxO3y, data=ozone)
 
 coef(lm.model)
+str(lm.model)
 #Ecuacion: maxO3 = 12.24 -0.01T9 + 2.22T12 + 0.56T15 - 
 #                  2.19Ne9 - 0.42N12 + 0.18Ne15 + 
 #                  0.94Wx9 + 0.03Wx12 + 0.42Wx15 + 0.35max03y
 
 
+### ==========================================================
+## 4: Aleatorios
+numbers <- read.csv("aleatorios.txt", header=F)
+summary(numbers)
+numbers.fix <- subset(numbers, V1 < 1)
+summary(numbers.fix)
 
+## Prueba chi-cuadrada
+# Paso 1: Dividir en intervalos
+bins <- apply(matrix(0:10), 1, function(i) i/10)
+numbers.table <- table(cut(as.matrix(numbers.fix),
+                           breaks=bins, right=F))
+numbers.df <- as.data.frame(numbers.table)
+# Paso 2: Frecuencia Esperada
+numbers.df$FE <- nrow(numbers.fix) / 10
+# Paso 3: La frecuencia Observada esta en la columna "Freq"
+# Paso 4: Valor de Chi
+numbers.df$Chi <- ((numbers.df$FE - numbers.df$Freq) ^ 2) / numbers.df$FE
+# Paso 5: Suma de los valores Cho
+chi.sq <- sum(numbers.df$Chi)
+# Paso 6: Chi en valor en tablas con alfa=0.05 y n-1 g.l.
+chi.sq.2 <- qchisq(p=0.95, df=9)
+# Verificar
+chi.sq < chi.sq.2
+
+## Prueba Kolmogorov-Smirnov
+# Paso 1: Ordenar los numeros
+numbers.sort <- data.frame(x=sort(as.matrix(numbers.fix)))
+# Paso 2: Distribucion Acumulada
+numbers.sort$DA <- apply(matrix(1:nrow(numbers.sort)), 1, 
+                         function(i) i / nrow(numbers.sort))
+# Paso 3: Diferencia del valor - la distribucion
+numbers.sort$KS <- numbers.sort$DA - numbers.sort$x
+# Paso 4: Valor observado
+ds <- max(abs(numbers.sort$KS))
+# No hay implementacion de la distribucion KS en R.
+# segun http://www.real-statistics.com/statistics-tables/kolmogorov-smirnov-table/
+ds.2 <- 1.36 / sqrt(nrow(numbers.sort))
+
+ds < ds.2
